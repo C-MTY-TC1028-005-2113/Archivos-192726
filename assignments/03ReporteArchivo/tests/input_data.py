9e54b123a8332cc80c173767cdce334bbfb7580b
roque63 <roque@itesm.mx>
2021-10-12 10:38:58 -0500

    Update input_data.py

--- assignments/03ReporteArchivo/tests/input_data.py
+++ assignments/03ReporteArchivo/tests/input_data.py
@@ -9,16 +9,7 @@ input_values = [
     # Test case 1
     (
         [],
-        ["1  Lupita     21   ITC",
-          "2  Lolita     3    LAE",
-          "3  Marita     12   ITD",
-          "4  Popeye     30   IMD",
-          "5  Gladys     14   ITD",
-          "6  Juvia      18   ITC",
-        "7  Graciela   20   IMD",
-        "8  Silvia     22   LAE",
-        "9  Luis       23   LAE",
-        "10 Ramón      22   LAE",
+        [
         "ITC 2",
         "LAE 4",
         "ITD 2",
