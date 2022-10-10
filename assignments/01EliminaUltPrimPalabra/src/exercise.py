1a1a381ca986a22d470c64f4cb116c5ed49e48e8
roque63 <roque@itesm.mx>
2022-10-09 22:08:41 -0500

    Update exercise.py

--- assignments/01EliminaUltPrimPalabra/src/exercise.py
+++ assignments/01EliminaUltPrimPalabra/src/exercise.py
@@ -1,12 +1,7 @@
 import os.path
 
 def elimina_primera_ultima_palabra_str(linea):
-    if linea.count(" ") >= 2:
-        pos = linea.index( " ")
-        pos2 = linea.rindex(" ")
-        extrae = linea[pos + 1:pos2]
-        print(extrae)
-
+    
     return extrae
 
 def main():
@@ -14,9 +9,7 @@ def main():
     folder = "assignments/01EliminaUltPrimPalabra/src/"
     with open(os.path.join(folder,"Data.txt")) as archEnt :
         with open(os.path.join(folder,"nuevasFrases.txt"), "w") as archSal :
-            for linea in archEnt:
-                archSal.write(elimina_primera_ultima_palabra_str(linea) )
-
+            
 
 if __name__=='__main__':
     main()
