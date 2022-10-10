c519ebf0640ec9e93f889c3ac5ddd37b9cc7c379
Ma. Guadalupe Roque <roque@itesm.mx>
2022-10-09 22:04:49 -0500

    solucion

--- assignments/01EliminaUltPrimPalabra/src/exercise.py
+++ assignments/01EliminaUltPrimPalabra/src/exercise.py
@@ -1,16 +1,21 @@
 import os.path
 
 def elimina_primera_ultima_palabra_str(linea):
+    if linea.count(" ") >= 2:
+        pos = linea.index( " ")
+        pos2 = linea.rindex(" ")
+        extrae = linea[pos + 1:pos2]
+        print(extrae)
 
-
-    return nuevaLinea
+    return extrae
 
 def main():
     #escribe tu código abajo de esta línea
     folder = "assignments/01EliminaUltPrimPalabra/src/"
     with open(os.path.join(folder,"Data.txt")) as archEnt :
         with open(os.path.join(folder,"nuevasFrases.txt"), "w") as archSal :
-            pass
+            for linea in archEnt:
+                archSal.write(elimina_primera_ultima_palabra_str(linea) )
 
 
 if __name__=='__main__':
